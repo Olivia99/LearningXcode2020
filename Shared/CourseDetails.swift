99acfc0fb1a7da518ed9c9a21b6d8bb2566cc06a
Jieying Yang <jieying.yang@cca.edu>
2020-07-28 00:06:34 -0700

    Imported swift files from Designn+Code. Added Visual Effect Blur when click on cards

--- Shared/CourseDetails.swift
+++ Shared/CourseDetails.swift
@@ -11,17 +11,37 @@ struct CourseDetails: View {
     var course:Course = courses[0]
     var namespace: Namespace.ID
     
+    #if os(iOS)
+    var cornerRadius: CGFloat = 22
+    #else
+    var cornerRadius: CGFloat = 0
+    #endif
+    
     var body: some View {
+      
+        #if os(iOS)
+        content
+        .edgesIgnoringSafeArea(.all)
+        #else
+        content
+        #endif
+    }
+    
+    var content: some View{
         VStack {
             ScrollView {
-                CourseItem(course: course)
+                CourseItem(course: course, cornerRadius: 0)
                     .matchedGeometryEffect(id: course.id, in: namespace)
                     .frame(height:300)
                 
                 
                 VStack {
-                    ForEach(0 ..< 20) { item in
-                        CourseRow()
+                    ForEach(courseSections) { item in
+                        /*
+                         pass course data to component "CourseRow"
+                         */
+                        CourseRow(item: item)
+                        Divider()
                     }
                 }
                 .padding()
@@ -30,17 +50,15 @@ struct CourseDetails: View {
             
         }
         .background(Color("Background 1"))
-        .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
         .matchedGeometryEffect(id: "container\(course.id)", in: namespace)
-        
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct CourseDetails_Previews: PreviewProvider {
     /*
-    because "course" has a defult value of courses[0], so you don't need to declare it here. However, you do need to declear "namespace since it doesn't have deful value.
-    */
+     because "course" has a defult value of courses[0], so you don't need to declare it here. However, you do need to declear "namespace since it doesn't have deful value.
+     */
     @Namespace static var namespace
     
     static var previews: some View {
--- Shared/CourseView.swift
+++ Shared/CourseView.swift
@@ -16,64 +16,98 @@ struct CourseView: View {
     
     var body: some View{
         ZStack {
-            ScrollView {
-                LazyVGrid(
-                    columns: [
-                        GridItem(.adaptive(minimum: 160), spacing:16)
-                    ], spacing: 16
-                ){
-                    ForEach(courses) { item in
-                        VStack {
-                            CourseItem(course: item)
-                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                                .frame( height: 250)
-                                .onTapGesture {
-                                    withAnimation(.spring()){
-                                        show.toggle()
-                                        selectedItem = item
-                                        isDisable = true
-                                    }
-                               
-                                }
-                                .disabled(isDisable)
-                        }
-                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-                        
-                    }
-                    
-                }
-                .padding(16)
-                .frame(maxWidth: .infinity)
-            }
+            #if os(iOS)
+            content
+                .navigationBarHidden(true)
+            fullContent
+                .background(VisualEffectBlur())
+                .edgesIgnoringSafeArea(.all)
+            #else
+            content
+            fullContent
+                
+                    .background(VisualEffectBlur())
+                    .edgesIgnoringSafeArea(.all)
             
-            if selectedItem != nil {
-                ZStack(alignment: .topTrailing)
-                {
-                    /*
-                     passing "course" and "namespace" value to CourseDetails
-                     */
-                    CourseDetails(course: selectedItem!, namespace: namespace)
-                    
-                    
-                    CloseButton()
-                        .padding(.trailing, 16)
-                        .onTapGesture {
-                            withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration:0)){
-                                show.toggle()
-                                selectedItem = nil
-                                DispatchQueue.main.asyncAfter(deadline: .now()+0.5) {
-                                    isDisable = false
-                                }
-                            }
-                        }
-                }
-                .zIndex(2)
-            }
+            #endif
+            
             
         }
+        .navigationTitle("Course")
         
         
     }
+    
+    
+    var content: some View {
+        
+        ScrollView {
+            LazyVGrid(
+                columns: [
+                    GridItem(.adaptive(minimum: 160), spacing:16)
+                ], spacing: 16
+            ){
+                ForEach(courses) { item in
+                    VStack {
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame( height: 250)
+                            .onTapGesture {
+                                withAnimation(.spring()){
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisable = true
+                                }
+                                
+                            }
+                            .disabled(isDisable)
+                    }
+                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
+                    
+                }
+                
+            }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    /*
+     You typically use ViewBuilder as a parameter attribute for child view-producing closure parameters, allowing those closures to provide multiple child views.
+     */
+    @ViewBuilder
+   
+    var fullContent : some View {
+        
+        if selectedItem != nil {
+            ZStack(alignment: .topTrailing)
+            {
+                /*
+                 passing "course" and "namespace" value to CourseDetails
+                 */
+                CourseDetails(course: selectedItem!, namespace: namespace)
+                
+                
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration:0)){
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now()+0.5) {
+                                isDisable = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth:.infinity)
+            
+        }
+        
+    }
+    
 }
 
 struct CourseView_Previews: PreviewProvider {
